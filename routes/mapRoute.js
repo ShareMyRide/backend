a42352f5ba358fb10f9f2bfc61c9759ee9d74443
kaveeshabasnayaka <139634637+kaveeshabasnayaka@users.noreply.github.com>
2025-01-01 16:24:35 +0000

    create fetchRoute function in mapRoute.js

--- routes/mapRoute.js
+++ routes/mapRoute.js
@@ -29,4 +29,32 @@ router.post('/route', async (req, res) => {
     }
   });
 
+  const fetchRoute = async () => {
+    if (!startLocation || !endLocation) {
+      Alert.alert('Error', 'Please enter both start and end locations.');
+      return;
+    }
+  
+    try {
+      const response = await axios.post('http://localhost:3000/route', {
+        start: startLocation.split(',').map(Number),
+        end: endLocation.split(',').map(Number),
+      });
+  
+      const routeCoordinates = response.data.features[0].geometry.coordinates.map(
+        ([longitude, latitude]) => ({ latitude, longitude })
+      );
+  
+      setCoordinates(routeCoordinates);
+      setMarkers([
+        { latitude: routeCoordinates[0].latitude, longitude: routeCoordinates[0].longitude },
+        { latitude: routeCoordinates[routeCoordinates.length - 1].latitude, longitude: routeCoordinates[routeCoordinates.length - 1].longitude },
+      ]);
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Error', 'Failed to fetch the route.');
+    }
+  };
+
+
 module.exports=router;
