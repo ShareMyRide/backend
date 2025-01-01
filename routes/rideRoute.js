cff1b763cbe408335b89aeb5c7b49dbc7e462e34
kaveeshabasnayaka <139634637+kaveeshabasnayaka@users.noreply.github.com>
2025-01-01 15:42:20 +0000

    create rideRoute.js with basic structure

---
+++ routes/rideRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const Ride = require('../models/Ride');
+
+router.get('/',async(req,res)=>{
+    
+    const result = await Ride.find();
+
+    if(result){
+        res.status(200).json(result)
+    }
+    else{
+        res.status(404).send("Ride not found")
+    }
+})
+
+module.exports=router;
