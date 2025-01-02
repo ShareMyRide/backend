bf1244789c19422a21a7950a84a249af08961f60
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-02 15:03:19 +0530

    create the feedback in routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const Feedback = require('../models/Feedback');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const auth = require('../Security/auth');
+const secretKey = 'project@shareMyRide';
 
