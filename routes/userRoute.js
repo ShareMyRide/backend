840d8d3a2501bd5e2aa9210676cb51a204aaf24b
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-02 15:06:07 +0530

    submit feedback in userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,3 +7,24 @@ const jwt = require('jsonwebtoken');
 const auth = require('../Security/auth');
 const secretKey = 'project@shareMyRide';
 
+// Submit feedback
+router.post('/feedback', auth, async (req, res) => {
+    const { rideId, rating, comment, feedbackType, name, telephone, kilometersTravelled, timeSpent } = req.body;
+    try {
+        const feedback = new Feedback({
+            userId: req.user.userId,
+            rideId,
+            rating,
+            comment,
+            feedbackType,
+            name,
+            telephone,
+            kilometersTravelled,
+            timeSpent
+        });
+        const savedFeedback = await feedback.save();
+        res.status(200).json(savedFeedback);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
